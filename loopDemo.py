695fc3d5a897bde14f645525530de6bf3bf72a59
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-27 13:34:37 -0400

    Updated from Brython Server: 9/27/2017 1:34:42 PM

--- loopDemo.py
+++ loopDemo.py
@@ -7,6 +7,19 @@
 for i in range(0,5):
     print('I <3 Computer Science')"""
 
+"""
 #print the numbers from 1 to 20
 for i in range(1,21):
-    print(i)
+    print(i) """
+    
+"""
+#print out the odd nubers from 13 to 27 (%2 ==1 for odds and %2 == 0 for evens)
+#third number tells how many to go up each time --> can do a negative number too
+for i in range(13, 28, 2):
+    print(i) """
+    
+#add up numbers from 1 to 5
+total = 0
+for i in range (1,6):
+    total = total + i
+print(total)
