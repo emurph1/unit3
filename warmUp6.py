babbecab6df6269c7d6013440a1914c38a317755
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-28 14:23:02 -0400

    Updated from Brython Server: 9/28/2017 2:23:10 PM

--- warmUp6.py
+++ warmUp6.py
@@ -2,8 +2,10 @@
 #2017-09-28
 #warmUp6.py 
 
-for i in range(0,10):
-    i += 1
+for i in range(1,11):
     print(i, 'mississippi')
 
-
+i = 1
+while i <= 10:
+    print(i, 'mississippi')
+    i += 1
