86adc967697fea6e180efd3b2060a2d33849cff2
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-29 14:57:43 -0400

    Updated from Brython Server: 9/29/2017 2:57:43 PM

--- divisors.py
+++ divisors.py
@@ -2,7 +2,10 @@
 #2017-09-29
 #divisors.py -
 
-i = int(input('Enter a number: '))
-while i <=  :
-    print(
+num = int(input('Enter a number: '))
+i = 1
+while i <= num :
+    if num%i == 0:
+        print(i)
+    i += 1
     
