74ba90d3dbb57412935a90349a128ba690da8e35
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-29 14:32:44 -0400

    Updated from Brython Server: 9/29/2017 2:32:44 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,10 @@
+#Emily Murphy
+#2017-09-29
+#hitchhiker.py - 
 
+
+for i in range(1,4):
+    question = input('Ask a question or quit: ')
+    print('42')
+    if question == 'quit':
+        break
