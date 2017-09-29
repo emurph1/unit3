eb938d1409aea9b4ed91d2710e5acbdec9a09e96
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-29 08:43:37 -0400

    Updated from Brython Server: 9/29/2017 8:43:37 AM

--- printStars.py
+++ printStars.py
@@ -2,7 +2,7 @@
 #2017-09-28
 #printStars.py -
 
-rows = 11
+rows = int(input('Enter number of rows you want: '))
 for i in range(rows):
-    print(' '*(11-i-1) + '*'*(2*i+1))
+    print(' '*(rows-i-1) + '*'*(2*i+1))
     
