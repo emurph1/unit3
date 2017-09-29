375d341c11bcc0cbbcbbbbe906a17945c8b33004
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-29 14:43:53 -0400

    Updated from Brython Server: 9/29/2017 2:43:53 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,14 @@
+#Emily Murphy
+#2017-09-29
+#numberGuessingGame.py -
 
+
+for i in range(1, 101):
+    guess = int(input('Enter a guess: '))
+    if guess < 63:
+        print('Too low')
+    elif guess > 63:
+        print('Too high')
+    elif guess == 63:
+        print('You got it in', i, 'tries')
+        break
