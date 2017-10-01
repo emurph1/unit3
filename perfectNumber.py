d14918bde1fb73f96f164cfcf96f3ddda652e582
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-30 21:59:22 -0400

    Updated from Brython Server: 9/30/2017 9:59:22 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -8,7 +8,7 @@ while i <= num:
     i += 1
     if num%i == 0:
         total = (i)
-        if total == num:
+        elif total == num:
             print('Perfect')
 else:
     print('Not Perfect')
