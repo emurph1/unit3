38d449265ebc70acaff4f8a5a790d15d09a093d8
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-09-30 21:21:41 -0400

    Updated from Brython Server: 9/30/2017 9:21:41 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,3 +1,13 @@
 #Emily Murphy
 #2017-09-30
 #perfectNumber.py - tells whether number is perfect or not
+
+num = int(input('Enter a number: '))
+i = 1
+while 1 <= num:
+    divisors = (num%1 == 0)
+    total = divisors + divisors
+    if total == num:
+        print('Perfect')
+    else:
+        print('Not Perfect')
