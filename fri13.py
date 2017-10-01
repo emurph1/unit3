f960f52280456b60db75cd99c986b4e16efdc0ad
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-01 14:20:29 -0400

    Updated from Brython Server: 10/1/2017 2:20:31 PM

--- fri13.py
+++ fri13.py
@@ -1,7 +1,4 @@
 #Emily Murphy
-#2017-09-30
+#2017-10-1
 #fri13.py - prints out dates for the next 10 fridays the 13ths
 
-import calendar
-calendar.setfirstweekday(calendar.SUNDAY)
-print(calendar.weekday(2017,10,1))
