fb020b9636a541f6a9f7e9da025d7f6ca9ddeb92
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-02 13:47:35 -0400

    Updated from Brython Server: 10/2/2017 1:47:39 PM

--- baseConverter.py
+++ baseConverter.py
@@ -6,7 +6,8 @@ num = int(input('Enter a base-10 number: '))
 base = int(input('What base would you like to convert to? ')) 
 if base == 2:
     for i in range(0,num):
-        print((num%10)*(2*i))
+        num1 = i%10
+        print(num1 * (2*i))
         
     
 
