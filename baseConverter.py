008a99690a34d0d3065dea0a2e3f3230cb651e96
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-02 12:09:01 -0400

    Updated from Brython Server: 10/2/2017 12:09:05 PM

--- baseConverter.py
+++ baseConverter.py
@@ -3,5 +3,8 @@
 #baseConverter.py - input a number then convert it to any base from base 2 to 16
 
 num = int(input('Enter a base-10 number: '))
-
+base = int(input('What base would you like to convert to? ')) 
+if b
+num1 = (num % 10)
+final1 = (num // 10)
 
