e88369ca1f81d059493de931dd7428d0ab4597e2
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-02 18:49:00 -0400

    Updated from Brython Server: 10/2/2017 6:48:59 PM

--- fri13.py
+++ fri13.py
@@ -9,8 +9,8 @@ today = date.today().day, date.today().month, date.today().year
 print(today)
 day = weekday(date.today().year,date.today().month, 13)
 i = 0
-while i < 1:
-    day == 4 and date.today().day == 13:
+while i <= 7:
+    if day == 4 and date.today().day == 13:
         print(today)
         i += 1
         
