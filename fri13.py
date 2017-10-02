39cd5ebac5ea0424f026963f5a34281e5c1bc238
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-02 14:38:03 -0400

    Updated from Brython Server: 10/2/2017 2:38:06 PM

--- fri13.py
+++ fri13.py
@@ -2,3 +2,7 @@
 #2017-10-02
 #fri13.py - prints out dates for the next 10 fridays the 13ths
 
+from datetime import date
+
+today = date.today(02).day, date.today(10).month, date.today(2017).year
+print(today)
