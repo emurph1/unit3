99c5fec02242cc4b3e98ca5117f5b2730c0cb481
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-02 14:24:06 -0400

    Updated from Brython Server: 10/2/2017 2:24:09 PM

--- warmUp8.py
+++ warmUp8.py
@@ -2,7 +2,8 @@
 #2017-10-02
 #warmUp8.py - find sum of all integers less than 100,000 that are divisible by 3, 10, and 17
 
+total = 0
 for i in range(1,100000):
     if i % 3 == 0 and i % 10 == 0 and i % 17 == 0:
-        print(i)
+        total += i
     
