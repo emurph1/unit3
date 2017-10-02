c0427b8b261087542d7a8d686f27c23fedc70219
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-02 14:44:35 -0400

    Updated from Brython Server: 10/2/2017 2:44:38 PM

--- fri13.py
+++ fri13.py
@@ -3,7 +3,8 @@
 #fri13.py - prints out dates for the next 10 fridays the 13ths
 
 from datetime import date
+from calendar import weekday
 
 today = date.today().day, date.today().month, date.today().year
 print(today)
-weekday = (2017,10,13)
+weekday(2017,10,13)
