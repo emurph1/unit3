77113f13b1b0a1cb23d3ad78f992aa9224905ae2
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-03 12:19:42 -0400

    Updated from Brython Server: 10/3/2017 12:19:45 PM

--- fri13.py
+++ fri13.py
@@ -9,12 +9,12 @@ today = date.today().year, date.today().month, date.today().day
 year = date.today().year
 month = date.today().month
 day = 13
-fri = weekday(date.today().year,date.today().month, day)
+fri = weekday(date.today().year,date.today().month, day) + today
 i = 1
 while i <= 10:
     i+=1
     fridate = (year, month, day)
     if day == 13 and fri == 4:
         print(fridate)
-        
+
 
