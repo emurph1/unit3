2c209c6e5d236df9b0f8264163bdb5c24150f7b3
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-03 13:12:40 -0400

    Updated from Brython Server: 10/3/2017 1:12:43 PM

--- usa.py
+++ usa.py
@@ -3,8 +3,18 @@
 #usa.py - use loops to make usa flag
 
 from ggame import *
+red = Color(0xFF0000,1)
+white = Color(0xFFFFFF,1)
+blue = Color(0x0000FF,1)
+
+whiteOutline = LineStyle(5, white)
+redRectangle = RectangleAsset(200, 200, whiteOutline, red)
+whiteRectangle = RectangleAsset(500, 200, whiteOutline, white)
+
 i = 0
 while i<=13:
+    i+=1
+    
     
 
 App().run()
