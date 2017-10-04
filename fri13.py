8e095e6bf9601c1d3237c6aa5d14b8263426c5ef
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-04 14:26:43 -0400

    Updated from Brython Server: 10/4/2017 2:26:44 PM

--- fri13.py
+++ fri13.py
@@ -8,12 +8,12 @@ from calendar import weekday
 year = date.today().year
 month = date.today().month
 day = date.today().day
-fri = weekday(date.today().year,date.today().month, 13 + 1)
+fri = weekday(date.today().year,date.today().month, 13)
 
 i = 1
 while i <= 10 and month < 13:
     i+=1
-    fridate = (year, month, day)
+    fridate = (year, month, 13 + 1)
     if fri == 4:
         print(fridate)
         
