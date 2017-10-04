d53ee7f5b27b9bfe337f4ef14a6bffea21c0ad8f
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-04 12:20:22 -0400

    Updated from Brython Server: 10/4/2017 12:20:22 PM

--- usa.py
+++ usa.py
@@ -6,10 +6,13 @@ from ggame import *
 red = Color(0xFF0000,1)
 white = Color(0xFFFFFF,1)
 blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
 
 whiteOutline = LineStyle(5, white)
+blackOutline = LineStyle(10, black)
+
 redRectangle = RectangleAsset(780, 50, whiteOutline, red)
-whiteRectangle = RectangleAsset(780, 50, whiteOutline, white)
+whiteRectangle = RectangleAsset(780, 50, blackOutline, white)
 
 i = 0
 while i<=7:
