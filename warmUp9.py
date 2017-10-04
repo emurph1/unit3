a0c1f25c52846ffe4040e2d2f98f59f74433afe6
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-04 13:45:16 -0400

    Updated from Brython Server: 10/4/2017 1:45:17 PM

--- warmUp9.py
+++ warmUp9.py
@@ -1,1 +1,9 @@
+#Emily Murphy
+#2017-10-04
+#warmUp9.py - asks for word, print out with the vowels capitalized
 
+word = input('Enter word: ')
+vowels = 'i' or  'e' or 'o' or 'u' or 'a'
+for ch in word:
+    if word == vowels
+    print(text.upper())
