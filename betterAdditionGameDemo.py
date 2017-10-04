a5d87002f907a913c7aeaf47bbac853755a01100
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-04 13:34:43 -0400

    Updated from Brython Server: 10/4/2017 1:34:44 PM

--- betterAdditionGameDemo.py
+++ betterAdditionGameDemo.py
@@ -7,7 +7,7 @@ from random import randint
 numCorrect = 0
 while numCorrect < 5:
     num1 = randint(-10,10)
-    num2 - randint(-10,10)
+    num2 = randint(-10,10)
     question = 'What is ' + str(num1) + '+' + str(num2) + '?'
     answer = int(input(question))
     break
