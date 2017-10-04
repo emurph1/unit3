8ef502c9de45d861aa174201a1a61477dd8e5c8a
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-04 12:14:15 -0400

    Updated from Brython Server: 10/4/2017 12:14:15 PM

--- usa.py
+++ usa.py
@@ -8,13 +8,13 @@ white = Color(0xFFFFFF,1)
 blue = Color(0x0000FF,1)
 
 whiteOutline = LineStyle(5, white)
-redRectangle = RectangleAsset(200, 600, whiteOutline, red)
+redRectangle = RectangleAsset(780, 50, whiteOutline, red)
 whiteRectangle = RectangleAsset(500, 200, whiteOutline, white)
 
 i = 0
 while i<=7:
     i+=1
-    Sprite(i**(redRectangle), (600,200))
+    Sprite(redRectangle, (300,0))
     
     
     
