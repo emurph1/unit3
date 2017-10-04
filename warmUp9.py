05d71ce6e13eecc319c7320ef700714533320936
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-04 13:58:08 -0400

    Update warmUp9.py

--- warmUp9.py
+++ warmUp9.py
@@ -4,7 +4,7 @@
 
 word = input('Enter word: ')
 for ch in word:
-    if ch == 'a' or ch == 'e' or ch == 'i' or ch =='o' or ch =='u':
+    if ch == 'a' or ch == 'e' or ch == 'i' or ch =='o' or ch =='u': #can also say ch in 'aeiouAEIOU'
         print(ch.upper())
     else:
         print(ch)
