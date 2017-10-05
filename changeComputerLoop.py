60a51b7069462c101ac4c0a5fe9fe1cf71154171
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 15:50:24 -0400

    Updated from Brython Server: 10/5/2017 3:50:26 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -3,14 +3,11 @@
 #changeComputerLoop.py - asks for number of cents and outputs amount of each coin needed
 
 cents = int(input('Enter number of cents: '))
-quarters = (cents//25)
-dimes = ((cents - (25 * quarters))//10)
-nickels = ((cents - ((10 * dimes) + (25 * quarters)))//5)
-pennies = ((cents - ((5 * nickels) + (10 * dimes) + (25 * quarters)))//1)
-i = 0
-while i <= cents:
-    i += 1
-print(quarters)
-print(dimes)
-print(nickels)
-print(pennies)
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
+i = 1
+while i <=4:
+    if cents-(25*i)== 0:
+        print('Quarters:', i)
