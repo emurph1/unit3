c699234030622e3fb61f398db1a2dce218fff281
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 14:37:49 -0400

    Updated from Brython Server: 10/5/2017 2:37:52 PM

--- warmUp10.py
+++ warmUp10.py
@@ -1,1 +1,15 @@
+#Emily Murphy
+#2017-10-06
+#warmUp10.py - make wallpaper
 
+from ggame import *
+
+blue = Color(0x0000FF,1)
+white = Color(0xFFFFFF,0)
+
+whiteOutline = LineStyle(15, white)
+
+blueRectangle = RectangleAsset(500, 200, whiteOutline, blue)
+
+
+App().run()
