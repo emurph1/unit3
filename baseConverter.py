2309873c5df3a391ade7c310144e9d92442f867f
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 14:53:43 -0400

    Updated from Brython Server: 10/5/2017 2:53:46 PM

--- baseConverter.py
+++ baseConverter.py
@@ -4,33 +4,7 @@
 
 num = int(input('Enter a base-10 number: '))
 base = int(input('What base would you like to convert to? ')) 
-if base == 2:
-    sigis
-elif base == 3:
-    sdfsd
-elif base == 4:    
-        
-elif base == 5:    
 
-elif base == 6:
+    
 
-elif base == 7:    
-    
-elif base == 8:   
-    
-elif base == 9:    
-    
-elif base == 10:
-    
-elif base == 11:        
-    
-elif base == 12:        
-    
-elif base == 13:        
-    
-elif base == 14:        
-    
-elif base == 15:        
-    
-elif base == 16:        
     
