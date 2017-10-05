58a8242c6916636a7db16ccccf6be41d350ca775
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 15:56:33 -0400

    Updated from Brython Server: 10/5/2017 3:56:35 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -8,7 +8,9 @@ dime = 10
 nickel = 5
 penny = 1
 i = 1
-while i <=4:
+while i <= 4:
     if cents-(25*i)== 0:
         print('Quarters:', i)
-    i += 1
+while i <= 10:
+    if cents -(10*i) == 0:
+        print('Dimes:'), i)
