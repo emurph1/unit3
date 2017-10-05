7a6771dcd7c0f207ab082686204a709a495824d9
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 16:16:49 -0400

    Updated from Brython Server: 10/5/2017 4:16:51 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -4,18 +4,24 @@
 
 cents = int(input('Enter number of cents: '))
 i = 1
+j = 1
+c = 1
+d = 1
 while i <= 4:
     i += 1
+    j += 1
+    c += 1
+    d += 1
     quarter = cents-(25*i)
     if quarter >= 0:
-      dime = quarter - (10*i)
+      dime = quarter - (10*j)
       if dime >= 0:
-          nickel = dime - (5*i)
+          nickel = dime - (5*c)
           if nickel >= 0:
-              penny = nickel -(1*i)
+              penny = nickel -(1*d)
              
-print('Quarters:', i - dime)                  
-print('Nickels:',i - nickel)           
-print('Dimes:', i- penny)
-print('Pennies:', i)
+print('Quarters:', i)                  
+print('Nickels:',j)           
+print('Dimes:', c)
+print('Pennies:', d)
               
