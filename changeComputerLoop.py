adfc34d0f081ab68d2082240d1260f7c8a53207f
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 20:42:54 -0400

    Updated from Brython Server: 10/5/2017 8:42:53 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -7,17 +7,7 @@ i = 1
 while i <= 4:
     i += 1
     totalq = cents -(25*i)
-    if totalq >= 0:
-        print(totalq)
-        totald = totalq -(10*i)
-        if totald >= 0:
-            print(totald)
-            totaln = totald - (5*i)
-            if totaln >= 0:
-                print(totaln)
-                totalp = totaln - (1*i)
-                if totalp >= 0:
-                    print(totalp)
+    
 print('Quarters:', i)
 print('Dimes:', i)
 print('Nickels:', i)
