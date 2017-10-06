f9b954586b488db9186d005df0a547d781796649
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 20:51:18 -0400

    Updated from Brython Server: 10/5/2017 8:51:18 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -3,7 +3,19 @@
 #changeComputerLoop.py - asks for number of cents and outputs amount of each coin needed
 
 cents = int(input('Enter number of cents: '))
-totalq = cents -(25*i)
-print(totalq)
+totalq = cents/25
+totald = totalq/10
+totaln = totald/5
+totalp = totaln/1
+i = 0
+while i == 4:
+    i += 1
+    if totalq != 0:
+        print(totald)
+        if totald != 0:
+            print(totaln)
+            if totaln != 0:
+                print(totalp)
+    
     
     
