9c42c5967267b5e09a53ad9482a0c27032a8949a
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-05 20:40:46 -0400

    Updated from Brython Server: 10/5/2017 8:40:46 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -6,16 +6,16 @@ cents = int(input('Enter number of cents: '))
 i = 1
 while i <= 4:
     i += 1
-    totalq = cents -(25*(cents/25))
+    totalq = cents -(25*(cents//25))
     if totalq >= 0:
         print(totalq)
-        totald = totalq -(10*(totalq/10))
+        totald = totalq -(10*(totalq//10))
         if totald >= 0:
             print(totald)
-            totaln = totald - (5*(totald/5))
+            totaln = totald - (5*(totald//5))
             if totaln >= 0:
                 print(totaln)
-                totalp = totaln - (1*(totaln/1))
+                totalp = totaln - (1*(totaln//1))
                 if totalp >= 0:
                     print(totalp)
 print('Quarters:', i)
