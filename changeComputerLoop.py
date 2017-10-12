0fdd511a1aa66d9303ecfd2c6dd599b010d1e39e
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-12 14:10:41 -0400

    Updated from Brython Server: 10/12/2017 2:10:38 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,7 +1,12 @@
 #Emily Murphy
-#2017-10-10
+#2017-10-12
 #changeComputerLoop.py - asks for number of cents and outputs amount of each coin needed
 
 cents = int(input('Enter number of cents: '))
 
-    
+i = 1
+while i <= cents:
+    cents - (25*i)
+    i +=1
+    if cents < 0:
+        
