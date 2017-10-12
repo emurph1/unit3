1eea3bc70de1437de5902ec32ebeba858b5e6751
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-12 14:18:01 -0400

    Updated from Brython Server: 10/12/2017 2:17:58 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -4,9 +4,26 @@
 
 cents = int(input('Enter number of cents: '))
 
-i = 1
-while i <= cents:
-    cents - (25*i)
-    i +=1
-    if cents < 0:
-        
+q = 0
+d = 0
+n = 0
+p = 0
+
+while cents >= 25:
+    cents = cents - 25
+    q += 1
+while cents >= 10:
+    cents = cents - 10
+    d += 1
+while cents >= 5:
+    cents = cents - 5
+    n +=1
+while cents >= 1:
+    cents = cents - 1
+    p +=1
+    
+print('Quarters:', q)
+print('Dimes:', d)
+print('Nickels:', n)
+print('Pennies:', p)
+
