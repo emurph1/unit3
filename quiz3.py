d10fa9939102b92200ced26243abee9b1102bdb5
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-13 14:22:59 -0400

    Updated from Brython Server: 10/13/2017 2:23:00 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,22 @@
+#Emily Murphy
+#2017-10-13
+#quiz3.py -
 
+for i in range(-5,6):
+    print(i)
+
+i = 18    
+while i <= 32:
+    print(i)
+    i += 1
+    
+total = 0
+for i  in range(13, 332):
+    if i%2 != 0:
+        total += i
+print(total)
+
+while True:
+    word = input('Enter a word')
+    if ch in word == 'z':
+        break
